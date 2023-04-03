4422356beb821c3bb3c65c7de5ccd8231d59d700
saljeshh <saljesh2020@gmail.com>
2023-04-03 16:53:32 +0545

    finished product

---
+++ src/components/ProductReviews.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductReviewCard from "../components/ProductReviewCard.js"
+import "../styles/ProductReviews.css"
+
+const ProductReviews = ({productReviews}) => {
+  return (
+    <div className="ProductReviews">
+        {productReviews.map((item,index)=>(
+            <ProductReviewCard price={item.price} name={item.name} image={item.image} review={item.review} />
+        ))}
+    </div>
+  )
+}
+
+export default ProductReviews
